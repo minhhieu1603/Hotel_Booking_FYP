d86e5efbf8a0f6df240d2d0dd011baf6821d3718
macOS <os@MacBook-Pro-cua-os.local>
2024-07-12 17:30:45 +0700

    first commit

---
+++ facilities.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>MH Hotel - FACILITIES</title>
+    
+    <!-- Links Design -->
+    <?php require('inc/links.php'); ?>
+    <style>
+      .pop:hover{
+        border-top-color: var(--teal) !important;
+        transform: scale(1.03);
+        transition: all 0.3s;
+      }
+    </style>
+
+</head>
+  <body class="bg-light">
+   
+  <!-- Header Design -->
+  <?php include ('inc/header.php'); ?>
+
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3"> 
+      ipsum dolor sit amet consectetur, adipisicing elit. Officiis nobis suscipit 
+      dolor odit maxime earum rerum sunt <br> iusto explicabo. Eum molestiae dicta, quae corporis 
+      quibusdam assumenda laboriosam iste praesentium dolor.
+    </p>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <?php
+        $res = selectAll('facilities');
+        $path = FACILITIES_IMG_PATH;
+
+        while($row = mysqli_fetch_assoc($res)){
+          echo<<<data
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+              <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                  <img src="$path$row[icon]" width="40px">
+                  <h5 class="m-0 ms-3">$row[name]</h5>
+                </div> 
+                <p>$row[description]</p>
+              </div>
+            </div>
+          data;
+        }
+      ?>
+    </div>
+  </div>
+
+
+  <!-- Footer Design -->
+  <?php require('inc/footer.php'); ?>
+
+  
+
+  </body>
+</html>

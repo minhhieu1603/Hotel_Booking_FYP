4866106f1b65aa63374f824b8e859ad013f58f58
minh hieu <hieu0902243343@gmail.com>
2024-07-18 01:46:22 +0700

    manage_images_remove_rooms

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -6,6 +6,7 @@ define('SITE_URL', 'http://127.0.0.1/MHHotelBooking/');
 define('ABOUT_IMG_PATH', SITE_URL.'images/about/');
 define('CAROUSEL_IMG_PATH', SITE_URL.'images/carousel/');
 define('FACILITIES_IMG_PATH', SITE_URL.'images/facilities/');
+define('ROOMS_IMG_PATH', SITE_URL.'images/rooms/');
 
 //backend upload process needs this data
 
@@ -13,6 +14,7 @@ define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'].'/MHHotelBooking/images/')
 define('ABOUT_FOLDER','about/');
 define('CAROUSEL_FOLDER','carousel/');
 define('FACILITIES_FOLDER','facilities/');
+define('ROOMS_FOLDER','rooms/');
 
 function adminLogin()
 {

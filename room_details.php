9f48e6561d2a3aa5a741d0e8810b60029db999c1
minh hieu <hieu0902243343@gmail.com>
2024-09-02 13:19:23 +0700

    Shutdown Feature Website_Admin

--- room_details.php
+++ room_details.php
@@ -165,9 +165,11 @@
                   </div>
                 area;
 
-                echo<<<book
+                if(!$settings_r['shutdown']){
+                  echo<<<book
                   <a href="#" class="btn w-100 text-white custom-bg shadow-none mb-1">Book Now</a>
-                book;
+                  book;
+                }
 
             ?>
           </div>

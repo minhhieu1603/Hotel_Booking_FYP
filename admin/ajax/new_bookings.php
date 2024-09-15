11f113422bc9fd29630606021822419f230b4e1d
minh hieu <hieu0902243343@gmail.com>
2024-09-15 16:07:26 +0700

    New Booking Management Page_Cancel Booking_Admin

--- admin/ajax/new_bookings.php
+++ admin/ajax/new_bookings.php
@@ -49,7 +49,7 @@ if (isset($_POST['get_bookings'])) {
                             <i class='bi bi-check2-square'></i> Assign Room
                         </button>
                         <br>
-                        <button type='button' class='mt-2 btn btn-outline-danger btn-sm fw-bold shadow-none'>
+                        <button type='button' onclick='cancel_booking($data[booking_id])' class='mt-2 btn btn-outline-danger btn-sm fw-bold shadow-none'>
                             <i class='bi bi-trash'></i> Cancel Booking
                         </button>
                     </td>
@@ -72,85 +72,18 @@ if (isset($_POST['assign_room']))
     
     $values = [1,$frm_data['room_no'],$frm_data['booking_id']];
 
-    $res = update($query,$values,'isi');
+    $res = update($query,$values,'isi'); // it will update 2 rows so it will return 2
 
     echo ($res==2) ? 1 : 0;  //1 = true; 2 = false
 }
 
+if (isset($_POST['cancel_booking'])) {
+    $frm_data = filteration($_POST);
 
+    $query = "UPDATE `booking_order` SET `booking_status`=?, `refund`=? WHERE `booking_id`=?";
+    $values = ['cancelled',0,$frm_data['booking_id']];
+    $res = update($query,$values,'sii');
 
+    echo $res;
+}
 
-
-
-
-
-
-
-
-
-// if (isset($_POST['remove_user'])) {
-//     $frm_data = filteration($_POST);
-
-//     $res = delete("DELETE FROM `user_cred` WHERE `id`=? AND `is_verified`=?", [$frm_data['user_id'], 0], 'ii');
-
-//     if ($res) {
-//         echo 1;
-//     } else {
-//         echo 0;
-//     }
-// }
-
-// if (isset($_POST['search_user'])) {
-//     $frm_data = filteration($_POST);
-
-//     $query = "SELECT * FROM `user_cred` WHERE `name` LIKE ?";
-
-//     $res = select($query, ["%$frm_data[name]%"], 's');
-//     $i = 1;
-//     $path = USERS_IMG_PATH;
-
-//     $data = "";
-
-//     while ($row = mysqli_fetch_assoc($res)) {
-//         $del_btn = "<button type='button' onclick='remove_user($row[id])' class='btn btn-danger shadow-none btn-sm'>
-//                             <i class='bi bi-trash'></i>
-//                         </button>";
-
-//         $verified = "<span class='badge bg-warning'><i class='bi bi-x-lg'></i></span>";
-
-//         if ($row['is_verified']) {
-//             $verified = "<span class='badge bg-success'><i class='bi bi-check-lg'></i></span>";
-//             $del_btn = "";
-//         }
-
-//         $status = "<button onclick='toggle_status($row[id],0)' class='btn btn-dark btn-sm shadow-none'>active</button>";
-
-//         if (!$row['status']) {
-//             $status = "<button onclick='toggle_status($row[id],1)' class='btn btn-danger btn-sm shadow-none'>inactive</button>";
-//         }
-
-//         $date = date("d-m-Y", strtotime($row['datentime']));
-
-//         $data .= "
-//                 <tr>
-//                     <td>$i</td>
-//                     <td>
-//                         <img src='$path$row[profile]' width='55px'>
-//                         <br>
-//                         $row[name]
-//                     </td>
-//                     <td>$row[email]</td>
-//                     <td>$row[phonenum]</td>
-//                     <td>$row[address] | $row[pincode]</td>
-//                     <td>$row[dob]</td>
-//                     <td>$verified</td>
-//                     <td>$status</td>
-//                     <td>$date</td>
-//                     <td>$del_btn</td>
-//                 </tr>
-//             ";
-//         $i++;
-//     }
-
-//     echo $data;
-// }
--- admin/scripts/new_bookings.js
+++ admin/scripts/new_bookings.js
@@ -47,42 +47,30 @@ assign_room_form.addEventListener('submit',function(e){
     xhr.send(data);
 });
 
-// function remove_user(user_id)
-// {
-//     if(confirm("Are you sure, you want to remove this user?"))
-//     {
-//         let data = new FormData();
-//         data.append('user_id', user_id);
-//         data.append('remove_user', '');
+function cancel_booking(id)
+{
+    if(confirm("Are you sure, you want to cancel this booking?"))
+        {
+            let data = new FormData();
+            data.append('booking_id', id);
+            data.append('cancel_booking', '');
 
-//         let xhr = new XMLHttpRequest();
-//         xhr.open("POST", "ajax/users.php",true);
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/new_bookings.php",true);
 
-//         xhr.onload = function()
-//         {
-//             if(this.responseText == 1){
-//                 alert('success', 'User Removed!');
-//                 get_users();  
-//             }
-//             else{
-//                 alert('error', 'User removal failed!');  
-//             }
-//         }
-//         xhr.send(data);       
-//     }
-// }
-
-// function search_user(username){
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", "ajax/users.php",true);
-//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-//     xhr.onload = function(){
-//         document.getElementById('users-data').innerHTML = this.responseText;
-//     }
-
-//     xhr.send('search_user&name='+username);
-// }
+            xhr.onload = function()
+            {
+                if(this.responseText == 1){
+                    alert('success', 'Booking Cancelled!');
+                    get_bookings();  
+                }
+                else{
+                    alert('error', 'Server Down!');  
+                }
+            }
+            xhr.send(data);       
+        }
+}
 
 window.onload = function(){
     get_bookings();

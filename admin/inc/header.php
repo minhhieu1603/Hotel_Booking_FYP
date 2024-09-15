549322198b3cea2bdedaa78d96007b193dd6b01e
minh hieu <hieu0902243343@gmail.com>
2024-09-15 20:34:17 +0700

    Refund Bookings_Admin

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -25,6 +25,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link text-white" href="new_bookings.php">New Bookings</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="refund_bookings.php">Refund Bookings</a>
+                                </li>
                             </ul>
                         </div>
                     </li>
